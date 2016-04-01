f647eb8864daa5f3a9b945bdf17be29ccd0d235f
Rashad Saleh <rashad.saleh@gmail.com>
2016-04-01 19:37:25 +0300

    Update CheckMockLocationsEnabled.java

--- src/android/CheckMockLocationsEnabled.java
+++ src/android/CheckMockLocationsEnabled.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
 * This class echoes a string called from JavaScript.
 */
-public class Echo extends CordovaPlugin {
+public class CheckMockLocationsEnabled extends CordovaPlugin {
 
 @Override
 public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
