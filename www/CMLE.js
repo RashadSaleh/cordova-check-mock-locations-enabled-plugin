bee334933fe13e53048531cfe90f2b0ae665f91d
Rashad Saleh <rashad.saleh@gmail.com>
2016-04-01 22:26:30 +0300

    Update CMLE.js

--- www/CMLE.js
+++ www/CMLE.js
@@ -1,28 +1,30 @@
-function CMLE() {
-}
-
-CMLE.prototype.check = function (callback) {
-    cordova.exec(
-        function(result) {
-            if (result == 1) {
-                callback(true);
-            } else {
-                callback(false);
-            }
-        }, 
-        function(error) {
-            console.log(error);
-        },
-        "CheckMockLocationsEnabled", "check", []);
-};
-
-CMLE.install = function () {
-    if (!window.plugins) {
-        window.plugins = {};
+cordova.define("cordova/plugin/CMLE", function(require, exports, module) {
+    function CMLE() {
     }
-
-    window.plugins.cml = new CMLE();
-    return window.plugins.cml;
-};
-
-cordova.addConstructor(CMLE.install);
+    
+    CMLE.prototype.check = function (callback) {
+        cordova.exec(
+            function(result) {
+                if (result == 1) {
+                    callback(true);
+                } else {
+                    callback(false);
+                }
+            }, 
+            function(error) {
+                console.log(error);
+            },
+            "CheckMockLocationsEnabled", "check", []);
+    };
+    
+    CMLE.install = function () {
+        if (!window.plugins) {
+            window.plugins = {};
+        }
+    
+        window.plugins.cml = new CMLE();
+        return window.plugins.cml;
+    };
+    
+    cordova.addConstructor(CMLE.install);
+});
