b54ad78bb539e4ddd1bbec362317416fe0c4ec85
Rashad Saleh <rashad.saleh@gmail.com>
2016-04-01 19:30:29 +0300

    Create CheckMockLocationsEnabled.java

---
+++ src/android/CheckMockLocationsEnabled.java
@@ -0,0 +1,35 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+* This class echoes a string called from JavaScript.
+*/
+public class Echo extends CordovaPlugin {
+
+@Override
+public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("check")) {
+        String message = args.getString(0);
+        this.check(callbackContext);
+        return true;
+    }
+    return false;
+}
+
+private void check(CallbackContext callbackContext) {
+  try {
+      if (Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+          callbackContext.success(true);
+      } else {
+          callbackContext.success(false);
+      }
+  } catch (Exception e) {
+    callbackContext.error(e.getMessage());
+  }
+}
